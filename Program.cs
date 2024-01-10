19a5f53066b5602ccfa54ab37298960009326590
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-10 11:28:16 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GAGitHubBasics_StephanieLopez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Professor!");
+        }
+    }
+}
