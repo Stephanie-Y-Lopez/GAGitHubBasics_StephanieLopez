0b3cd15c2d793f4a673b4546e7be318436cc1ce7
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-10 11:31:33 -0800

    Done!

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor!");
+            Console.WriteLine("My name is Stephanie Lopez and I would like to learn more fundimentals about computer science!");
         }
     }
 }
